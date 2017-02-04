931902563ad7f831db453cc08c59f5445c55f0ea
Michael  Taylor <email": null>
2017-02-04 15:36:17 -0600

    mid rebase commit

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,9 @@
 class Location < ApplicationRecord
   validates :latitude, :longitude, presence: true
+  validates :latitude, :longitude, numericality: true
 
   has_many :sound_treks
   has_many :playlists, through: :sound_treks
   has_many :trekkers, through: :playlists, source: :creator
+
 end
