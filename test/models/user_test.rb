f12e63dd2c3550a0bf0bd2cd58a0de204c43740f
Ashley Yang <email": null>
2017-02-04 11:59:08 -0600

    Add rspec-rails 2.99, deleted folder, changed gemfile to upgrade rspec-rails to 3.0. Regenerated spec setup. Commented out code in .travis.yml Need to fix CI configuration

--- test/models/user_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UserTest < ActiveSupport::TestCase
-  test "should not save without name or email" do
-    user = User.new
-    assert_not user.valid?
-    assert_equal [:name, :email], user.error.keys
-  end
-end
