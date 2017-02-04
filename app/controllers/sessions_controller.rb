a1967376dfbaf99c22eddaaf35367fe8336e4dfc
Ashley Yang <email": null>
2017-02-04 17:42:00 -0600

    Set session_id on login. Add welcome message to user profile page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     session[:omniauth] = auth
-    # .except("extra")
     user = User.sign_in_from_omniauth(auth)
+    session[:user_id] = user.id
     redirect_to root_url, notice: "SIGNED IN"
   end
 
