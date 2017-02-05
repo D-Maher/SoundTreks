1522b39224e326d40889fb650469a8791df9be7b
Ashley Yang <email": null>
2017-02-05 15:07:04 -0600

    Display logged in user uid in header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   protect_from_forgery with: :exception
-
-  private
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  helper_method :current_user
-
 end
 
