3dd957fb2a328c1b50e182ab9a8c3a065f3138ae
Michael  Taylor <email": null>
2017-02-04 18:10:56 -0600

    Take out commented debugging p's

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -4,18 +4,11 @@ class PlaylistsController < ApplicationController
   end
 
   def show
-    p "I GOT TO THE SHOW PAGE!"
     RSpotify.authenticate(ENV['spotify_id'], ENV['spotify_secret'])
     user = User.find_by(:id => session[:user_id])
     spotify_user = RSpotify::User.find(user.spotify_id)
-    # spotify_user.playlists.each { |x| p x.name }
-    # base_url = "https://embed.spotify.com/?theme=white&uri=spotify:trackset:My Playlist:"
     playlist = RSpotify::Playlist.find(user.spotify_id, '2y0EFH0qtraCl8fbfhI5IJ')
     base_url = "https://embed.spotify.com/?uri=spotify:user:#{user.spotify_id}:playlist:#{playlist.id}"
-    # playlist.tracks.each do |track|
-    #   base_url += track.id
-    #   p base_url
-    # end
     respond_to do |format|
       format.json { render layout: false, json: {:base_url => base_url }}
     end
@@ -29,6 +22,4 @@ class PlaylistsController < ApplicationController
 
   end
 end
-
-"https://embed.spotify.com/?ur=spotify:user: :playlist:"
 
