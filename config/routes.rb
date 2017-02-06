5e1352d8abbfd56872768db6a40a68cf547c515f
Ashley Yang <email": null>
2017-02-06 12:39:17 -0600

    Update routes so that rating is nested under soundtrek

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,14 @@ resources :locations do
   resources :sound_treks, except: [:new]
 end
 
-resources :sound_treks, only: [:show]
+resources :sound_treks, only: [:show] do
+  resources :ratings, only: [:create, :edit]
+end
 
 get 'auth/:provider/callback', to: "sessions#create"
 delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
 
 resources :users
-resources :ratings, only: [:create, :edit]
 
 resources :playlists, only: [:new, :create, :show]
 
