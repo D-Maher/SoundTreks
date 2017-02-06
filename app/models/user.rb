605694bf5c062fc9c3b127a46583faf5129b39c9
Ashley Yang <email": null>
2017-02-06 11:00:41 -0600

    add debugging

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,14 @@ class User < ApplicationRecord
   has_many :sound_treks, foreign_key: "trekker_id"
 
   def self.sign_in_from_omniauth(auth)
+    p "=" * 15
+    p auth['provider']
     # user_id = auth["extra"]["raw_info"]["id"]
     find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
   end
 
   def self.create_user_from_omniauth(auth)
+    p "I GOT TO CREATE USER FROM OMNI"
     create(
       provider: auth['provider'],
       uid: auth['uid'],
