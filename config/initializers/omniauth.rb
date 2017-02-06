c2957f9fe2cf36aae18079357a4ed31795ceaaa0
Ashley Yang <email": null>
2017-02-06 09:29:12 -0600

    Add provider_ignores_state true to omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,7 @@
 # require 'omniauth-spotify'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['spotify_id'], ENV['spotify_secret']
-  end
+  provider :spotify, ENV['spotify_id'], ENV['spotify_secret'] {
+    provider_ignores_state: true
+  }
+end
