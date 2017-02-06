4dadb7c0ee6412a3455254354c00823a1affeaca
Ashley Yang <email": null>
2017-02-06 11:53:04 -0600

    Add rating model and migration for ratings

--- app/models/sound_trek.rb
+++ app/models/sound_trek.rb
@@ -2,4 +2,5 @@ class SoundTrek < ApplicationRecord
   validates :location_id, :title, presence: true
   belongs_to :trekker, class_name: "User"
   belongs_to :location, dependent: :destroy
+  has_many :ratings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,14 @@
 class User < ApplicationRecord
   has_many :sound_treks, foreign_key: "trekker_id"
+  has_many :ratings, foreign_key: "trekker_id"
 
   def self.sign_in_from_omniauth(auth)
     user = User.new
-    p "=" * 15
-    p auth['provider']
-    p "HERE IS A USER"
-    p user
     # user_id = auth["extra"]["raw_info"]["id"]
     find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
   end
 
   def self.create_user_from_omniauth(auth)
-    p "I GOT TO CREATE USER FROM OMNI"
     create(
       provider: auth['provider'],
       uid: auth['uid'],
@@ -20,5 +16,4 @@ class User < ApplicationRecord
       spotify_id: auth["extra"]["raw_info"]["id"]
       )
   end
-
 end
