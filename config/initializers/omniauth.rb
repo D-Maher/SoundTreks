2092f835c4658c0c5d6dd5676a8f3d269133e2db
Ashley Yang <email": null>
2017-02-06 09:30:46 -0600

    fix syntax error

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 # require 'omniauth-spotify'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['spotify_id'], ENV['spotify_secret'] {
+  provider :spotify, ENV['spotify_id'], ENV['spotify_secret'], {
     provider_ignores_state: true
   }
 end
