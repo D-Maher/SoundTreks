39a7d66bdef1969bb97089d747814610c0d461f5
Ashley Yang <email": null>
2017-02-06 11:10:14 -0600

    Add new debugging p

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ApplicationRecord
   has_many :sound_treks, foreign_key: "trekker_id"
 
   def self.sign_in_from_omniauth(auth)
+    user = User.new
     p "=" * 15
     p auth['provider']
+    p "HERE IS A USER"
+    p user
     # user_id = auth["extra"]["raw_info"]["id"]
     find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
   end
