dcc9dcfbe6a975ce6a656a16aa7d3be61ad8b1eb
Ashley Yang <email": null>
2017-02-06 10:39:02 -0600

    Change route to /auth/:provider/callback. Works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,11 @@ class SessionsController < ApplicationController
 include SessionsHelper
 
   def create
+    p "=" * 40
+    p "Im In CREATE!"
     auth = request.env["omniauth.auth"]
     session[:omniauth] = auth
+    p session['omniauth.state']
     user = User.sign_in_from_omniauth(auth)
     session[:user_id] = user.id
     redirect_to root_url, notice: "SIGNED IN"
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,5 @@
 # require 'omniauth-spotify'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['spotify_id'], ENV['spotify_secret'], {
-    provider_ignores_state: true
-  }
+  provider :spotify, ENV['spotify_id'], ENV['spotify_secret']
 end
