55ca373ce5159c81d3fcfb3c00aaea28682df892
Ashley Yang <email": null>
2017-02-07 17:44:32 -0600

    Add logic to create action for the rating controller so that creators cannot rate their own soundtrek

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -4,18 +4,21 @@ class RatingsController < ApplicationController
   def create
     @sound_trek = SoundTrek.find_by(id: params[:sound_trek_id])
 
-    if !rated_by_current_user?(@sound_trek) && !sound_trek_creator?
-      p "*" * 50
-      @rating = @sound_trek.ratings.new(rating_params)
-      @rating.update_attributes(sound_trek_id: params[:sound_trek_id], trekker_id: session[:user_id])
-      if @rating.save
-        flash[:rating_notice] = "Your rating has been recorded."
-        redirect_to sound_trek_path(@sound_trek)
+    if !sound_trek_creator?
+      if !rated_by_current_user?(@sound_trek)
+        @rating = @sound_trek.ratings.new(rating_params)
+        @rating.update_attributes(sound_trek_id: params[:sound_trek_id], trekker_id: session[:user_id])
+        if @rating.save
+          flash[:rating_notice] = "Your rating has been recorded."
+          redirect_to sound_trek_path(@sound_trek)
+        else
+         render file: 'public/404.html'
+        end
       else
-       render file: 'public/404.html'
+        redirect_to sound_trek_path(@sound_trek)
       end
-    elsif rated_by_current_user?(@sound_trek) && !sound_trek_creator?
-      p "=" * 50
+    else
+      flash[:creator_rating] = "You cannot rate your own SoundTrek."
       redirect_to sound_trek_path(@sound_trek)
     end
   end
