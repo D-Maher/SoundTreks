f3c7b55c1a235c3ffa2505767b6b4b73219883e2
Danny Maher <email": null>
2017-02-07 14:18:00 -0600

    Edit models to reflect drop of locations table, add geocoder methods to SoundTrek model, add dependent: :destroy to User model to destroy SoundTrek records associated with a destroyed User, deleted locations model

--- app/models/sound_trek.rb
+++ app/models/sound_trek.rb
@@ -1,8 +1,12 @@
 class SoundTrek < ApplicationRecord
-  validates :location_id, :title, presence: true
+  validates :location_id, :title, :latitude, :longitude, presence: true
+  validates :latitude, :longitude, numericality: true
+
   belongs_to :trekker, class_name: "User"
-  belongs_to :location, dependent: :destroy
   has_many :ratings
+
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
 
   def average_rating
     if num_of_ratings > 0
@@ -17,3 +21,14 @@ class SoundTrek < ApplicationRecord
   end
 
 end
+
+# class Location < ApplicationRecord
+#   validates :latitude, :longitude, presence: true
+#   validates :latitude, :longitude, numericality: true
+
+#   has_one :sound_trek
+
+#   reverse_geocoded_by :latitude, :longitude
+#   after_validation :reverse_geocode
+# end
+
