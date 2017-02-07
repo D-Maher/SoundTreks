86c579bcb583b1847b2d0630633548499a8411ea
Danny Maher <dmaher724@gmail.com>
2017-02-07 16:18:55 -0600

    Update sound_trek.rb

--- app/models/sound_trek.rb
+++ app/models/sound_trek.rb
@@ -21,14 +21,3 @@ class SoundTrek < ApplicationRecord
   end
 
 end
-
-# class Location < ApplicationRecord
-#   validates :latitude, :longitude, presence: true
-#   validates :latitude, :longitude, numericality: true
-
-#   has_one :sound_trek
-
-#   reverse_geocoded_by :latitude, :longitude
-#   after_validation :reverse_geocode
-# end
-
