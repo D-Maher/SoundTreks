a6a8a33b9a6e1d2a16a60fe20b8d1eb6155ce0f4
Ashley Yang <email": null>
2017-02-09 10:26:37 -0600

    Add about controller and about index to add an about page. Add link to nav bar to navigate to about page

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -7,10 +7,6 @@ class PlaylistsController < ApplicationController
     RSpotify.authenticate(ENV['spotify_id'], ENV['spotify_secret'])
     user = User.find_by(:id => session[:user_id])
     spotify_user = RSpotify::User.find(user.spotify_id)
-    # p "Playlist here!!!" * 5
-    # spotify_user.playlists.each do |playlist|
-    #   p playlist.id
-    # end
     playlist = RSpotify::Playlist.find(user.spotify_id, '2y0EFH0qtraCl8fbfhI5IJ')
     base_url = "https://embed.spotify.com/?uri=spotify:user:#{user.spotify_id}:playlist:#{playlist.id}"
     respond_to do |format|
